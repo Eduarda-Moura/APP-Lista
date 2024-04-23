5291348d19f6cc62ef303d86a13124c5523263bd
erick <erickschultz133@gmail.com>
2024-04-23 17:38:36 -0300

    Adicionadas as funcionalidades novas até o slide 14 (Passo 12).

--- app/src/main/java/com/pkg/lista/activity/MainActivity.java
+++ app/src/main/java/com/pkg/lista/activity/MainActivity.java
@@ -1,18 +1,25 @@
 package com.pkg.lista.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.pkg.lista.model.MyItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import moura.eduarda.erick.applista.R;
 
 public class MainActivity extends AppCompatActivity {
     static int NEW_ITEM_REQUEST = 1;
+    List<MyItem> itens = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +35,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(i, NEW_ITEM_REQUEST);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == NEW_ITEM_REQUEST) {
+            if (resultCode == Activity.RESULT_OK){
+                MyItem myItem = new MyItem();
+
+                myItem.title = data.getStringExtra("title");
+                myItem.description = data.getStringExtra("desc");
+                myItem.photo = data.getData();
+
+                itens.add(myItem);
+            }
+        }
     }
 }
--- app/src/main/java/com/pkg/lista/activity/NewItemActivity.java
+++ app/src/main/java/com/pkg/lista/activity/NewItemActivity.java
@@ -8,8 +8,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import moura.eduarda.erick.applista.R;
 
@@ -20,16 +22,46 @@ public class NewItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
 
         ImageButton imgCl = findViewById(R.id.imbCl);
         imgCl.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 photoPickerIntent.setType("image/*");
                 startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST);
+
+                if (photoSelected == null) {
+                    Toast.makeText(NewItemActivity.this, "É necessário selecionar uma imagem!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                EditText etTitle = findViewById(R.id.etTitle);
+                String title = etTitle.getText().toString();
+
+                if (title.isEmpty()) {
+                    Toast.makeText(NewItemActivity.this, "É necessário inserir um título", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                EditText etDesc = findViewById(R.id.etDesc);
+                String desc = etDesc.getText().toString();
+
+                if (desc.isEmpty()){
+                    Toast.makeText(NewItemActivity.this, "É necessário inserir uma descrição", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Intent i = new Intent();
+                i.setData(photoSelected);
+                i.putExtra("title", title);
+                i.putExtra("desc", desc);
+                setResult(Activity.RESULT_OK, i);
+                finish();
             }
         });
     }
